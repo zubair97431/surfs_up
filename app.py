2261de88763c31150526eca2f3c2e76ab4dc111f
Zubair Aslam <zubairaslam97@gmail.com>
2020-05-09 01:50:20 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-	return 'Hello world'
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+	prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+	results = session.query(Measurement.tobs).\
+		filter(Measurement.station == 'USC00519281').\
+		filter(Measurement.date >= prev_year).all()
+	temps = list(np.ravel(results))
+	return jsonify(temps)
+app.run()
+
+
+
 
